9ee12d9a5028afa9d35be66674cc42c8675e2376
kiluazen <kushalsokke@gmail.com>
2024-08-11 14:57:59 +0530

    google analytics added

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,28 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Header() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            window.gtag('config', 'G-D3SC1WNS4Q', {
+                page_path: url,
+            });
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
+    const handleResumeClick = () => {
+        window.gtag('event', 'resume_click', {
+            'event_category': 'engagement',
+            'event_label': 'Resume Download'
+        });
+    };
+
     return (
         <header className="relative"> 
   <div className="absolute top-0 left-4 md:left-[10rem] md:lg:left-[15rem] xl:left-[21rem] pt-5"><img src='/logo.webp' className="w-[80px]"/></div>
@@ -8,7 +32,8 @@ export default function Header() {
       <a className="hover:text-[#DA95DE] font-medium"  href='/'>Home</a>
       <a className="hover:text-[#DA95DE] font-medium" href='/about'>About</a>
       <a className="hover:text-[#DA95DE] font-medium" href='/blog'>Blog</a>
-      <a className="hover:text-[#DA95DE] font-medium" href='/resume.pdf' id = 'resume-clickeed'>Resume</a>
+      <a className="hover:text-[#DA95DE] font-medium" href='/products'>Products</a>
+      <a className="hover:text-[#DA95DE] font-medium" href='/resume.pdf' id='resume-clicked' onClick={handleResumeClick}>Resume</a>
     </div>
   </div>
   </div>
