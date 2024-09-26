29c9a6e0e84eca202fb0fc805f94dc6c796a9939
kiluazen <kushalsokke@gmail.com>
2024-09-26 16:23:34 +0530

    added simple analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import "../styles/global.css";
 import Head from "next/head";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-[#DFD7CF] min-h-screen flex flex-col">
@@ -11,6 +12,10 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/contemplation.ico" />
       </Head>
       <GoogleAnalytics gaId="G-D3SC1WNS4Q" />
+      {/* Add Simple Analytics script */}
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+     
+     
       <Header />
       <main className="flex-grow">
         <Component {...pageProps} />
