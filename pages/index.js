917eb3076dfb21534483754aa0a16ce2cb817a80
kiluazen <kushalsokke@gmail.com>
2025-06-16 18:37:57 +0530

    updated home page

--- pages/index.js
+++ pages/index.js
@@ -15,47 +15,39 @@ export default function Home() {
           <div className="flex-1 items-start flex flex-col gap-2 md:gap-4 pl-4 md:pl-0 py-2 md:pt-0">
             <div className="flex flex-col gap-1 md:gap-4 text-[1rem] md:text-[1.2rem]">
               <h1 className="text-[#525051] font-[Sora] text-[1.8rem] md:text-[2.5rem] not-italic font-bold leading-[120%]">
-                I'm Kushal
+               Hello! I'm Kushal
               </h1>
-              <p>I showcase my work, thoughts, and interests here to connect with awesome people across the internet</p>
+              <h3 className="text-[#525051] font-[Sora] text-[1.5rem] not-italic font-bold leading-[120%]">
+              Currently
+              </h3>
+              <ul></ul>
+                <li>Building <a href="https://hyphenbox.com" className="text-[#9B9692] hover:text-[#DA95DE]">hyphenbox</a> : a “second cursor” that guides users inside SaaS apps</li>
+                <li>Independent consultant : browser agents, memory layers, AI engineering </li>
+                <li>Always up for conversations about what you’re building</li>
               
               <p>
-                Checkout some <a href="/portfolio" className="text-[#9B9692] hover:text-[#DA95DE]">products</a> I have built 
+              What I've built: <a href="/portfolio" className="text-[#9B9692] hover:text-[#DA95DE]">[portfolio]</a>
               </p>
               <p>
-                I'd love to hear from you, let me know what you think.
+                I'd love to hear from you: <a
+                href="mailto:kushalsokke@gmail.com?subject=Coffee!"
+                className="text-[#9B9692] hover:text-[#DA95DE]"
+              >
+                [email]
+              </a>
               </p>
             </div>
-            <div className="flex flex-row gap-4">
-              <a
-                href="mailto:kushalsokke@gmail.com?subject=Coffee!"
-                className="underline text-[#9B9692] font-bold hover:text-[#DA95DE]"
-              >
-                Email
-              </a>
-              <a
-                href="https://twitter.com/KushalSM5"
-                className="underline text-[#9B9692] font-bold hover:text-[#DA95DE]"
-              >
-                Twitter
-              </a>
-              <a
-                href="https://github.com/kiluazen"
-                className="underline text-[#9B9692] font-bold  hover:text-[#DA95DE]"
-              >
-                Github
-              </a>
-            </div>
+  
             <div className="flex flex-col gap-2 md:flex-row md:gap-4">
               <div>
-                <span>Curated Lists :</span>
+                <span>Curated Media :</span>
               </div>
               <div className="flex flex-row gap-4">
                 <a
                   href="/books"
                   className="underline text-[#9B9692] hover:text-[#DA95DE]"
                 >
-                  Reading List
+                  Books
                 </a>
                 <a
                   href="/podcasts"
